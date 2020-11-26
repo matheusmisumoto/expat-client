a4a7be8a4f50f70d04fdc751fbe42ab03e634f95
Matheus Misumoto <49180175+matheusmisumoto@users.noreply.github.com>
2020-11-25 23:08:31 -0300

    Update RESTClientInterface.java

--- src/main/java/expat/rest/client/RESTClientInterface.java
+++ src/main/java/expat/rest/client/RESTClientInterface.java
@@ -9,7 +9,7 @@ public interface RESTClientInterface<T> {
 	public static final int STATUS_NOT_ACCEPTABLE = Response.Status.NOT_ACCEPTABLE.getStatusCode();
 	
     public static final String REST_WEBSERVICE_URL = 
-    	       "http://localhost:8081/api/";
+    	       "http://api-expat.herokuapp.com/api/";
     public static final String REST_CADASTRO_URL = "usuario/";
     public static final String REST_CIDADE_URL = "cidade/";
     public static final String REST_CUSTO_URL = "custo/";
