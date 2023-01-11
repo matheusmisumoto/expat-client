c23d27e58a1e09dee13ea5fdae6d0224ebec07f4
Matheus Misumoto <49180175+matheusmisumoto@users.noreply.github.com>
2023-01-10 23:44:30 -0300

    Refactor: change paths related to the API
    - Change deploy path on Procfile

--- src/main/java/expat/rest/client/RESTClientInterface.java
+++ src/main/java/expat/rest/client/RESTClientInterface.java
@@ -9,9 +9,9 @@ public interface RESTClientInterface<T> {
 	public static final int STATUS_NOT_ACCEPTABLE = Response.Status.NOT_ACCEPTABLE.getStatusCode();
 
     public static final String REST_WEBSERVICE_URL = System.getenv("API_URL");
-    public static final String REST_CADASTRO_URL = "api/usuario/";
-    public static final String REST_CIDADE_URL = "api/cidade/";
-    public static final String REST_CUSTO_URL = "api/custo/";
+    public static final String REST_CADASTRO_URL = "v1/usuario/";
+    public static final String REST_CIDADE_URL = "v1/cidade/";
+    public static final String REST_CUSTO_URL = "v1/custo/";
 
     public List<T> findAll();
     public T find(Long id);
